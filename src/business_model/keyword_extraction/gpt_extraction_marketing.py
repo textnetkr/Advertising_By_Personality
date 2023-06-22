acd029de7902721aaca694bda75071a90c683965
hipster4020 <hipster4020@gmail.com>
2023-06-22 19:01:51 +0900

    chatgpt 마케팅 정보, 소구점 정보 추출

--- src/business_model/keyword_extraction/gpt_extraction_marketing.py
+++ src/business_model/keyword_extraction/gpt_extraction_marketing.py
@@ -13,7 +13,7 @@ def main(cfg):
     try:
         # Data Load
         df = fm.load(cfg.PATH.for_gpt)
-        print(df.head())
+        df_ref = fm.load(cfg.PATH.EXT_GPT_MARKET)
 
         # OpenAI Api Key
         openai.api_key = cfg.OPENAI.OPENAI_API_KEY
@@ -25,10 +25,12 @@ def main(cfg):
 
         # GPT Extraction
         predict = []
-        start = 40
-        df_start = df.iloc[start:].copy()
+        start = df_ref.shape[0]
+        print(f"start: {start}")
+        df_temp = df.iloc[start:]
+        print(df_temp.head())
         print(f"{start}번째 행부터 시작!")
-        data = [[i[1]["type"], i[1]["input"]] for i in df_start.iterrows()]
+        data = [[i[1]["type"], i[1]["input"]] for i in df_temp.iterrows()]
         for k, i in tqdm(enumerate(data)):
             messages = [
                 {
@@ -37,8 +39,19 @@ def main(cfg):
                 },
                 {
                     "role": "user",
-                    "content": f"""'{i[1]}' 문장에서 아래 정보를 다음 키와 함께 JSON 형식으로 만들어줘: 마케팅 주체-marketing_entity, 타겟-marketing_target, 혜택 지급 조건-benefit_conditions, 혜택-benefits, 할인 수치-discount_figure, 프로모션 품목-promotional_items, 프로모션 장소-promotional_place, 이벤트 기간-event_period, 요일 정보-dow_information, 시즌 정보-season_information""",
-                    # : marketing_entity, marketing_target, benefit_conditions, benefits, discount_figure, promotional_items, promotional_place, event_period, dow_information, season_information: marketing_entity, marketing_target, benefit_conditions, benefits, discount_figure, promotional_items, promotional_place, event_period, dow_information, season_information
+                    "content": f"""{i[1]}\n
+                                    위 문장에서 아래 정보를 ',\n'를 기준으로 뽑아줘.
+                                    마케팅 주체 : ,\n
+                                    타겟 : ,\n
+                                    혜택 지급 조건 : ,\n
+                                    혜택 : ,\n
+                                    할인 수치 : ,\n
+                                    프로모션 품목 : ,\n
+                                    프로모션 장소 : ,\n
+                                    이벤트 기간 : ,\n
+                                    요일 정보 : ,\n
+                                    시즌 정보 : 
+                                """,
                 },
             ]
             result = generate_response(messages)
@@ -46,9 +59,7 @@ def main(cfg):
             print(f"원문 : {i[1]}")
             print(f"답변 : {result}")
             print("-" * 100)
-            res_dict = json.loads(result)
-            res_dict = {i[0]: i[1] for i in res_dict.items()}
-            predict.append(res_dict)
+            predict.append(result)
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -56,30 +67,85 @@ def main(cfg):
 
     finally:
         # Processing
-        df_temp = df_start.iloc[: len(predict)].copy()
-        df_pred = pd.DataFrame(predict)
-        print(f"df_temp : {df_temp.shape}")
-        print(f"df_pred : {df_pred.shape}")
-        df_result = pd.concat([df_temp, df_pred], axis=1)
+        df_temp = df.iloc[start : start + len(predict)].copy()
+        df_temp["marketing"] = predict
+        df_temp["marketing_temp"] = df_temp.marketing.apply(
+            lambda x: x.replace("마케팅 주체 : ", "")
+            .replace("타겟 : ", "")
+            .replace("혜택 지급 조건 : ", "")
+            .replace("혜택 : ", "")
+            .replace("할인 수치 : ", "")
+            .replace("프로모션 품목 : ", "")
+            .replace("프로모션 장소 : ", "")
+            .replace("이벤트 기간 : ", "")
+            .replace("요일 정보 : ", "")
+            .replace("시즌 정보 : ", "")
+        )
+        df_temp["marketing_temp"] = df_temp.marketing_temp.apply(
+            lambda x: x.replace("마케팅 주체: ", "")
+            .replace("타겟: ", "")
+            .replace("혜택 지급 조건: ", "")
+            .replace("혜택: ", "")
+            .replace("할인 수치: ", "")
+            .replace("프로모션 품목: ", "")
+            .replace("프로모션 장소: ", "")
+            .replace("이벤트 기간: ", "")
+            .replace("요일 정보: ", "")
+            .replace("시즌 정보: ", "")
+        )
+        df_temp["marketing_temp"] = df_temp.marketing_temp.str.replace("\n\n", "\n")
+        df_temp["marketing_temp"] = df_temp.marketing_temp.str.replace("\n", ",\n")
+        df_temp["marketing_temp"] = df_temp.marketing_temp.str.replace(",,", ",")
+        df_split_marketing = df_temp.marketing_temp.str.split(",\n", expand=True)
+        df_split_marketing_true = df_split_marketing[
+            df_split_marketing[0].str.contains(",\n")
+        ]
+        df_split_marketing_true = df_split_marketing_true[0].str.split(
+            ",\n", expand=True
+        )
+        df_split_marketing_false = df_split_marketing[
+            ~df_split_marketing[0].str.contains(",\n")
+        ]
+        df_split_marketing_temp = pd.concat(
+            [df_split_marketing_false, df_split_marketing_true]
+        )
+        # 마케팅 주체: marketing_entity, 타겟: marketing_target, 혜택 지급 조건: benefit_conditions, 혜택: benefits, 할인 수치: discount_figure, 프로모션 품목: promotional_items, 프로모션 장소: promotional_place, 이벤트 기간: event_period, 요일 정보: dow_information, 시즌 정보: season_information, 소구점: solicitation_point
+        df_split_marketing_temp.rename(
+            columns={
+                0: "marketing_entity",
+                1: "marketing_target",
+                2: "benefit_conditions",
+                3: "benefits",
+                4: "discount_figure",
+                5: "promotional_items",
+                6: "promotional_place",
+                7: "event_period",
+                8: "dow_information",
+                9: "season_information",
+            },
+            inplace=True,
+        )
+        df_temp.rename(columns={"input": "label"}, inplace=True)
+        df_temp = df_temp[["type", "label"]]
+        df_result = pd.concat([df_split_marketing_temp, df_temp], axis=1)
+        df_result.sort_index(ascending=True, inplace=True)
         df_result.fillna("없음", inplace=True)
-        df_result.replace("", "없음", inplace=True)
-        print(df_result.head())
 
         # save
         temp_dict = [
             {
-                "marketing_entity": row["marketing_entity"],
-                "marketing_target": row["marketing_target"],
-                "benefit_conditions": row["benefit_conditions"],
-                "benefits": row["benefits"],
-                "discount_figure": row["discount_figure"],
-                "promotional_items": row["promotional_items"],
-                "promotional_place": row["promotional_place"],
-                "event_period": row["event_period"],
-                "dow_information": row["dow_information"],
-                "season_information": row["season_information"],
+                "marketing_entity": row["marketing_entity"].strip(),
+                "marketing_target": row["marketing_target"].strip(),
+                "benefit_conditions": row["benefit_conditions"].strip(),
+                "benefits": row["benefits"].strip(),
+                "discount_figure": row["discount_figure"].strip(),
+                "promotional_items": row["promotional_items"].strip(),
+                "promotional_place": row["promotional_place"].strip(),
+                "event_period": row["event_period"].strip(),
+                "dow_information": row["dow_information"].strip(),
+                "season_information": row["season_information"].strip(),
                 "type": row["type"],
-                "label": row["input"],
+                "label": row["label"].strip(),
             }
             for _, row in df_result.iterrows()
         ]
--- src/business_model/keyword_extraction/gpt_extraction_selling.py
+++ src/business_model/keyword_extraction/gpt_extraction_selling.py
@@ -1,3 +1,4 @@
+import re
 import hydra
 import json
 import time
@@ -10,117 +11,106 @@ from pshmodule.utils import filemanager as fm
 
 @hydra.main(config_path="./", config_name="config")
 def main(cfg):
-    # try:
-    # Data Load
-    data = []
-    with open(cfg.PATH.for_selling, "r", encoding="utf-8") as f:
-        for line in f:
-            data.append(json.loads(line.rstrip("\n|\r")))
-    df = pd.DataFrame(data)
+    try:
+        # Data Load
+        data = []
+        with open(cfg.PATH.EXT_GPT_MARKET, "r", encoding="utf-8") as f:
+            for line in f:
+                data.append(json.loads(line.rstrip("\n|\r")))
+        df = pd.DataFrame(data)
 
-    # OpenAI Api Key
-    openai.api_key = cfg.OPENAI.OPENAI_API_KEY
-    model = "gpt-4"
+        # OpenAI Api Key
+        openai.api_key = cfg.OPENAI.OPENAI_API_KEY
+        # model = "gpt-3.5-turbo"
+        model = "gpt-4"
 
-    # $$7.64 - $6.61
-    def generate_response(messages: list) -> str:
-        response = openai.ChatCompletion.create(model=model, messages=messages)
-        return response["choices"][0]["message"]["content"]
+        def generate_response(messages: list) -> str:
+            response = openai.ChatCompletion.create(model=model, messages=messages)
+            return response["choices"][0]["message"]["content"]
 
-    # GPT Extraction
-    predict = []
-    start = 3500
-    df_start = df.iloc[start:3510]
+        # GPT Extraction
+        start = 0
+        df_start = df.iloc[start:5]
 
-    s_time = time.time()
-    print(df_start.head())
-    print(f"{start}번째 행부터 시작!")
+        s_time = time.time()
+        print(df_start.head())
+        print(f"{start}번째 행부터 시작!")
 
-    for k, i in enumerate(df_start.iterrows()):
-        messages = [
+        selling = []
+        selling_sent = []
+        market_info = []
+        for k, i in enumerate(df_start.iterrows()):
+            # selling_point
+            messages = [
+                {
+                    "role": "system",
+                    "content": mp.mbti["SELLING_SF"],
+                },
+                {
+                    "role": "user",
+                    "content": f"""{i[1]['label']}\n위 문장에 적합한 소구점을 만들어줘.\n
+                                소구점 : """,
+                },
+            ]
+
+            sell_result = generate_response(messages)
+            print(f"{start + k}번째 행")
+            print(f"원문 : {i[1]['label']}")
+            print(f"답변 : {sell_result}")
+            print("-" * 100)
+            selling.append(sell_result)
+
+            # selling_sent
+            messages = [
+                {
+                    "role": "system",
+                    "content": mp.mbti["SELLING_SENT_SF"],
+                },
+                {
+                    "role": "user",
+                    # "content": f"""{i[1]['label']}\n위 문장에 해당하는 마케팅 대상 : {i[1]['marketing_entity']}, 타겟 : {i[1]['marketing_target']}, 혜택 지급 조건 : {i[1]['benefit_conditions']}, 혜택 : {i[1]['benefits']}, 할인 수치 : {i[1]['discount_figure']}, 프로모션 품목 : {i[1]['promotional_items']}, 프로모션 장소 : {i[1]['promotional_place']}, 이벤트 기간 : {i[1]['event_period']}, 요일 정보 : {i[1]['dow_information']}, 시즌 정보 : {i[1]['season_information']}, 소구점 : {sell_result}을 반영하여 매력있는 광고 문구를 만들어줘.\n
+                    # 소구점이 반영된 광고 문구 : """,
+                    "content": f"""{i[1]['label']}\n위 문장에 해당하는 소구점 : {sell_result}을 반영하여 매력있는 광고 문구를 만들어줘.\n
+                                소구점이 반영된 광고 문구 : """,
+                },
+            ]
+
+            sell_sent = generate_response(messages)
+            print(f"{start + k}번째 행")
+            print(f"원문 : {i[1]['label']}")
+            print(f"답변 : {sell_sent}")
+            print("-" * 100)
+            selling_sent.append(sell_sent)
+
+            # marketing_info
+            market_info.append(
+                f"마케팅 대상 : {i[1]['marketing_entity']}, 타겟 : {i[1]['marketing_target']}, 혜택 지급 조건 : {i[1]['benefit_conditions']}, 혜택 : {i[1]['benefits']}, 할인 수치 : {i[1]['discount_figure']}, 프로모션 품목 : {i[1]['promotional_items']}, 프로모션 장소 : {i[1]['promotional_place']}, 이벤트 기간 : {i[1]['event_period']}, 요일 정보 : {i[1]['dow_information']}, 시즌 정보 : {i[1]['season_information']}"
+            )
+
+        math.factorial(100000)
+        e_time = time.time()
+        print(f"{e_time - s_time:.5f} sec")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # temp save
+        df_temp = df.iloc[start : start + len(selling)].copy()
+        df_temp["selling"] = selling
+        df_temp["sentence"] = selling_sent
+        df_temp["market_info"] = market_info
+        df_temp = df_temp[["label", "market_info", "selling", "sentence"]]
+        df_temp.rename(
             {
-                "role": "system",
-                "content": mp.mbti["SELLING_POINT"],
+                "label": "원문",
+                "market_info": "마케팅 정보",
+                "selling": "소구점",
+                "sentence": "소구점이 반영된 광고 문구",
             },
-            {
-                "role": "user",
-                "content": f"""{i[1]['label']}\n위 문장에 적합한 소구점을 찾고, 찾아낸 소구점을 반영해서 매력적인 광고 문구로 \n을 기준으로 만들어줘.\n
-                            소구점 : \n
-                            소구점이 반영된 광고 문구 : """,
-            },
-        ]
-
-        result = generate_response(messages)
-        print(f"{start + k}번째 행")
-        print(f"원문 : {i[1]['label']}")
-        print(f"답변 : {result}")
-        print("-" * 100)
-        predict.append(result)
-
-    math.factorial(100000)
-    e_time = time.time()
-    print(f"{e_time - s_time:.5f} sec")
-
-    # temp save
-    df["predict"] = predict
-    fm.save(cfg.PATH.temp_save, df)
-
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
-    #     print(f"{start + k}번째 행에서 오류!")
-    # finally:
-    #     # Processing
-    #     df_temp = df.iloc[start : start + len(predict)].copy()
-    #     df_temp["mbti"] = predict
-    #     df_temp["mbti"] = df_temp.mbti.str.replace("NT 성향 문구 : ", "")
-    #     df_temp["mbti"] = df_temp.mbti.str.replace("NT 성향 문구: ", "")
-    #     df_temp["mbti"] = df_temp.mbti.str.replace("NF 성향 문구 : ", "")
-    #     df_temp["mbti"] = df_temp.mbti.str.replace("NF 성향 문구: ", "")
-    #     # df_temp["mbti_temp"] = df_temp.mbti_temp.str.replace("\n\n", "\n")
-    #     # df_temp["mbti_temp"] = df_temp.mbti_temp.str.replace("\n", ",\n")
-    #     # df_temp["mbti_temp"] = df_temp.mbti_temp.str.replace(",,", ",")
-    #     # df_split_mbti = df_temp.mbti_temp.str.split(",\n", expand=True)
-    #     # df_split_mbti_true = df_split_mbti[df_split_mbti[0].str.contains(",\n")]
-    #     # df_split_mbti_true = df_split_mbti_true[0].str.split(",\n", expand=True)
-    #     # df_split_mbti_false = df_split_mbti[~df_split_mbti[0].str.contains(",\n")]
-    #     # df_split_mbti_temp = pd.concat([df_split_mbti_false, df_split_mbti_true])
-    #     # df_split_mbti_temp.rename(
-    #     #     columns={
-    #     #         0: "NT",
-    #     #         1: "NF",
-    #     #     },
-    #     #     inplace=True,
-    #     # )
-    #     # df_result = pd.concat([df_split_mbti_temp, df_temp], axis=1)
-    #     df_result = df_temp.copy()
-    #     df_result.sort_index(ascending=True, inplace=True)
-    #     df_result.fillna("없음", inplace=True)
-
-    #     # save
-    #     temp_dict = [
-    #         {
-    #             "mbti": row["mbti"].strip(),
-    #             "marketing_entity": row["marketing_entity"].strip(),
-    #             "marketing_target": row["marketing_target"].strip(),
-    #             "benefit_conditions": row["benefit_conditions"].strip(),
-    #             "benefits": row["benefits"].strip(),
-    #             "discount_figure": row["discount_figure"].strip(),
-    #             "promotional_items": row["promotional_items"].strip(),
-    #             "promotional_place": row["promotional_place"].strip(),
-    #             "event_period": row["event_period"].strip(),
-    #             "dow_information": row["dow_information"].strip(),
-    #             "season_information": row["season_information"].strip(),
-    #             "solicitation_point": row["solicitation_point"].strip(),
-    #             "type": row["type"],
-    #             "label": row["label"].strip(),
-    #         }
-    #         for _, row in df_result.iterrows()
-    #     ]
-    #     with open(eval(f"cfg.PATH.ext_gpt_mbti"), "a", encoding="utf-8") as f:
-    #         for line in temp_dict:
-    #             json_record = json.dumps(line, ensure_ascii=False)
-    #             f.write(json_record + "\n")
-    #     print("Save Done!")
+            inplace=True,
+        )
+        fm.save(cfg.PATH.temp_save, df_temp)
+        print("Save Done!")
 
 
 if __name__ == "__main__":
