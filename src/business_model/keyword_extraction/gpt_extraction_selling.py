dd84071b3b0fcee136282df8656d21cab3c085a9
hipster4020 <hipster4020@gmail.com>
2023-06-28 18:29:23 +0900

    소구점, 소구점 반영 광고 문구 추출

--- src/business_model/keyword_extraction/gpt_extraction_selling.py
+++ src/business_model/keyword_extraction/gpt_extraction_selling.py
@@ -1,4 +1,3 @@
-import re
 import hydra
 import json
 import time
@@ -11,6 +10,12 @@ from pshmodule.utils import filemanager as fm
 
 @hydra.main(config_path="./", config_name="config")
 def main(cfg):
+    start = 0
+    sell = []
+    sell_title = []
+    sell_content = []
+    market_info = []
+
     try:
         # Data Load
         data = []
@@ -18,32 +23,33 @@ def main(cfg):
             for line in f:
                 data.append(json.loads(line.rstrip("\n|\r")))
         df = pd.DataFrame(data)
-
-        df_temp = df.iloc[[956, 282, 2002, 2003, 2004], :].copy()
-        print(df_temp.head())
+        df_ref = fm.load(cfg.PATH.EXT_GPT_SELL)
 
         # OpenAI Api Key
         openai.api_key = cfg.OPENAI.OPENAI_API_KEY
-        # model = "gpt-3.5-turbo"
-        model = "gpt-4"
+        model = "gpt-3.5-turbo"
 
         def generate_response(messages: list) -> str:
-            response = openai.ChatCompletion.create(model=model, messages=messages)
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=messages,
+                # temperature=temperature,
+            )
             return response["choices"][0]["message"]["content"]
 
         # GPT Extraction
-        # start = 0
-        # df_start = df.iloc[start:5]
-
+        start = df_ref.shape[0]
+        df_start = df.iloc[start:].copy()
         s_time = time.time()
-        # print(df_start.head())
-        # print(f"{start}번째 행부터 시작!")
+        print(f"start: {start}")
+        print(df_start.head())
+        print(f"{start}번째 행부터 시작!")
 
-        selling = []
-        selling_sent = []
-        market_info = []
-        for k, i in enumerate(df_temp.iterrows()):
+        for k, i in enumerate(df_start.iterrows()):
             # selling_point
+            temp = i[1]["label"].split("\\\\")
+            title = temp[0]
+            content = temp[1].replace("\\", " ")
             messages = [
                 {
                     "role": "system",
@@ -51,18 +57,17 @@ def main(cfg):
                 },
                 {
                     "role": "user",
-                    "content": f"""{i[1]['label']}\n위 문장에 적합한 소구점을 만들어줘.\n
+                    "content": f"""'''제목 : {title}, 본문 : {content}''' 오직 소구점 찾는 방법에 있는 내용만 활용해서 삼중 따옴표 안의 제목과 본문 내용에 적합한 소구점을 찾아줘.
                                 소구점 : """,
                 },
             ]
 
-            sell_result = generate_response(messages)
-            # print(f"{start + k}번째 행")
-            print(f"{k}번째 행")
-            print(f"원문 : {i[1]['label']}")
-            print(f"답변 : {sell_result}")
+            result1 = generate_response(messages)
+            print(f"{start + k}번째 행")
+            print(f"제목 : {title}, 본문 : {content}")
+            print(f"답변 : {result1}")
             print("-" * 100)
-            selling.append(sell_result)
+            sell.append(result1.replace("소구점 : ", ""))
 
             # selling_sent
             messages = [
@@ -72,25 +77,29 @@ def main(cfg):
                 },
                 {
                     "role": "user",
-                    # "content": f"""{i[1]['label']}\n위 문장에 해당하는 마케팅 대상 : {i[1]['marketing_entity']}, 타겟 : {i[1]['marketing_target']}, 혜택 지급 조건 : {i[1]['benefit_conditions']}, 혜택 : {i[1]['benefits']}, 할인 수치 : {i[1]['discount_figure']}, 프로모션 품목 : {i[1]['promotional_items']}, 프로모션 장소 : {i[1]['promotional_place']}, 이벤트 기간 : {i[1]['event_period']}, 요일 정보 : {i[1]['dow_information']}, 시즌 정보 : {i[1]['season_information']}, 소구점 : {sell_result}을 반영하여 매력있는 광고 문구를 만들어줘.\n
-                    # 소구점이 반영된 광고 문구 : """,
-                    # "content": f"""{i[1]['label']}\n위 문장에 해당하는 요일 정보 : {i[1]['dow_information']}, 시즌 정보 : {i[1]['season_information']}, 소구점 : {sell_result}을 반영하여 매력있는 광고 문구를 만들어줘.\n
-                    # 소구점이 반영된 광고 문구 : """,
-                    "content": f"""{i[1]['label']}\n위 문장에 해당하는 소구점 : {sell_result}을 반영하여 매력있는 광고 문구를 만들어줘.\n
-                                소구점이 반영된 광고 문구 : """,
+                    # "content": f"""'''제목 : {title}, 본문 : {content}''' 삼중 따옴표 안의 마케팅 문구를 마케팅 주체 : '{i[1]['marketing_entity']}', 타겟 : '{i[1]['marketing_target']}', 혜택 지급 조건 : '{i[1]['benefit_conditions']}', 혜택 : '{i[1]['benefits']}', 할인 수치 : '{i[1]['discount_figure']}', 프로모션 품목 : '{i[1]['promotional_items']}', 프로모션 장소 : '{i[1]['promotional_place']}', 이벤트 기간 : '{i[1]['event_period']}', 요일 정보 : '{i[1]['dow_information']}', 시즌 정보 : '{i[1]['season_information']}', 소구점 : '{result1}'을 반영하여 매력있는 광고 제목과 광고 본문으로 변경해줘.
+                    # 결과는 다음 두 개의 키로 JSON 형식으로만 제공해줘: title, content""",
+                    # "content": f"""'''제목 : {title}, 본문 : {content}''' 삼중 따옴표 안의 제목과 본문에 요일 정보 : {i[1]['dow_information']}, 시즌 정보 : {i[1]['season_information']}, 소구점 : {result1}을 반영하여 매력있는 광고 제목과 광고 본문을 만들어줘.
+                    # 결과는 다음 두 개의 키로 JSON 형식으로만 제공해줘: title, content""",
+                    # "content": f"""'''제목 : {title}, 본문 : {content}''' 삼중 따옴표 안의 제목과 본문에 소구점 : {result1}을 반영하여 매력있는 광고 제목과 광고 본문을 만들어줘.
+                    # 결과는 다음 두 개의 키로 JSON 형식으로만 제공해줘: title, content""",
+                    "content": f"""'''제목 : {title}, 본문 : {content}''' 삼중 따옴표 안의 제목과 본문에 마케팅 주체 : '{i[1]['marketing_entity']}', 혜택 : '{i[1]['benefits']}', 소구점 : '{result1}'을 반영하여 매력있는 광고 제목과 광고 본문을 만들어줘.
+                    결과는 다음 두 개의 키로 JSON 형식으로만 제공해줘: title, content""",
                 },
             ]
 
-            sell_sent = generate_response(messages)
-            # print(f"{start + k}번째 행")
-            print(f"원문 : {i[1]['label']}")
-            print(f"답변 : {sell_sent}")
+            result2 = generate_response(messages)
+            print(f"{start + k}번째 행")
+            print(f"제목 : {title}, 본문 : {content}")
+            print(f"답변 : {result2}")
             print("-" * 100)
-            selling_sent.append(sell_sent)
+            result_json = json.loads(result2)
+            sell_title.append(result_json["title"])
+            sell_content.append(result_json["content"])
 
             # marketing_info
             market_info.append(
-                f"마케팅 대상 : {i[1]['marketing_entity']}, 타겟 : {i[1]['marketing_target']}, 혜택 지급 조건 : {i[1]['benefit_conditions']}, 혜택 : {i[1]['benefits']}, 할인 수치 : {i[1]['discount_figure']}, 프로모션 품목 : {i[1]['promotional_items']}, 프로모션 장소 : {i[1]['promotional_place']}, 이벤트 기간 : {i[1]['event_period']}, 요일 정보 : {i[1]['dow_information']}, 시즌 정보 : {i[1]['season_information']}"
+                f"마케팅 주체 : {i[1]['marketing_entity']}, 혜택 : {i[1]['benefits']}"
             )
 
         math.factorial(100000)
@@ -101,22 +110,55 @@ def main(cfg):
         print(f"An error occurred: {e}")
     finally:
         # temp save
-        # df_temp = df.iloc[start : start + len(selling)].copy()
-        df_temp["selling"] = selling
-        df_temp["sentence"] = selling_sent
-        df_temp["market_info"] = market_info
-        df_temp = df_temp[["label", "market_info", "selling", "sentence"]]
-        df_temp.rename(
+        df_temp = df.iloc[start : start + len(sell_content)].copy()
+        df_temp["use_market"] = market_info
+        df_temp["sell_point"] = sell
+        df_temp["sell_title"] = sell_title
+        df_temp["sell_content"] = sell_content
+
+        # save
+        temp_dict = [
             {
-                "label": "원문",
-                "market_info": "마케팅 정보",
-                "selling": "소구점",
-                "sentence": "소구점이 반영된 광고 문구",
-            },
-            inplace=True,
-        )
-        fm.save(cfg.PATH.temp_save, df_temp)
+                "marketing_entity": row["marketing_entity"],
+                "marketing_target": row["marketing_target"],
+                "benefit_conditions": row["benefit_conditions"],
+                "benefits": row["benefits"],
+                "discount_figure": row["discount_figure"],
+                "promotional_items": row["promotional_items"],
+                "promotional_place": row["promotional_place"],
+                "event_period": row["event_period"],
+                "dow_information": row["dow_information"],
+                "season_information": row["season_information"],
+                "type": row["type"],
+                "label": row["label"].strip(),
+                "use_market": row["use_market"],
+                "sell_point": row["sell_point"],
+                "sell_title": row["sell_title"],
+                "sell_content": row["sell_content"],
+            }
+            for _, row in df_temp.iterrows()
+        ]
+        with open(cfg.PATH.EXT_GPT_SELL, "a", encoding="utf-8") as f:
+            for line in temp_dict:
+                json_record = json.dumps(line, ensure_ascii=False)
+                f.write(json_record + "\n")
+
         print("Save Done!")
+
+        # Excel Save
+        # df_temp = df_temp[
+        #     ["label", "use_market", "sell_point", "sell_title", "sell_content"]
+        # ]
+        # df_temp.rename(
+        #     {
+        #         "label": "원문",
+        #         "market_info": "마케팅 정보",
+        #         "selling": "소구점",
+        #         "sentence": "소구점이 반영된 광고 문구",
+        #     },
+        #     inplace=True,
+        # )
+        # fm.save(cfg.PATH.temp_save, df_temp)
 
 
 if __name__ == "__main__":
