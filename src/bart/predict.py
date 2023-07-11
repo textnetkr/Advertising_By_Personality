2a963a66be6bab57790f465b48ea0fabb3ac9339
hipster4020 <hipster4020@gmail.com>
2023-07-11 11:12:22 +0900

    cj 문체 변환 kobart 학습

---
+++ src/bart/predict.py
@@ -0,0 +1,81 @@
+import re
+import hydra
+import torch
+from transformers import PreTrainedTokenizerFast, BartForConditionalGeneration
+from pshmodule.processing import processing as p
+
+
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+device = torch.device("cuda")
+
+
+@hydra.main(config_path="./", config_name="config")
+def main(cfg):
+    # tokenizer
+    tokenizer = PreTrainedTokenizerFast.from_pretrained(cfg.PATH.save_dir)
+
+    # model
+    model = BartForConditionalGeneration.from_pretrained(cfg.PATH.save_dir)
+    model.to(device)
+
+    # convert_to_other_unicode
+    nlp = p.Nlp()
+
+    texts = [
+        ["", "NT", "SF", "당신은 덧셈 천재입니까?\\\\덧셈 게임 참여 시 💰31만 포인트가 [TAG1]님 주머니로 쏘-옥"],
+        [
+            "가을맞이",
+            "ST",
+            "SF",
+            "10월이니까 1OOO포인트♬\\\\브랜드별 할인 받고 1천P 적립까지 더!\\알뜰하게 가을 옷 준비하려면 터치▶",
+        ],
+        [
+            "신학기",
+            "NF",
+            "NT",
+            "우리 아이 성적도 자신감도 홈런⚾\\\\그 유명한 초등학습 1위 [홈런]\\새학기를 맞아 무료 체험을 제공해요❣️ 역사 연대표와 포인트도 드리니까 부담없이 신청하세요😍",
+        ],
+        [
+            "어버이날",
+            "NT",
+            "ST",
+            "이번 어버이날 선물은 너로 정했다🎁\\\\지금 부모님 선물 구매하시면 2,OOOP를 사은품으로 드립니다💰 선물 사고 선물 받고🎵 꿩 먹고 알 먹고💗",
+        ],
+        [
+            "",
+            "ST",
+            "NF",
+            "7일 뒤면 사라지는 쿠폰...❗\\\\7/일/뒤 혜택 종료!! 지금 앱에 접속해서 미사용 쿠폰 체크✔️체크✔️",
+        ],
+    ]
+
+    for i in texts:
+        temp = str(i[1]).replace("\\\\", "\n\n").replace("\\", "\n")
+        print(f"바꿀 문구 : {temp}, 시즌 정보 : {i[0]}")
+        print(i[3])
+
+        text = f"<season>{i[0]}<ctrl1>{i[1]}<ctrl2>{i[2]}{tokenizer.sep_token}{nlp.convert_to_other_unicode(i[3])}"
+
+        encoded = tokenizer(text)
+        sample = {k: torch.tensor([v]).to(device) for k, v in encoded.items()}
+        with torch.no_grad():
+            pred = model.generate(
+                **sample,
+                penalty_alpha=0.6,
+                top_k=5,
+                max_length=512,
+                eos_token_id=tokenizer.eos_token_id,
+            )
+        print(f"바뀐 문구 : {i[2]}")
+        # 이모지로 역치환 - Predict 후에 적용해야 함
+        result = nlp.convert_emojis_in_text(
+            nlp.convert_to_python_unicode(
+                tokenizer.decode(pred[0], skip_special_tokens=True)
+            )
+        )
+        print(result.replace("\\\\", "\n\n").replace("\\", "\n"))
+        print("-" * 100)
+
+
+if __name__ == "__main__":
+    main()
--- src/business_model/t5_selling/predict.py
+++ src/business_model/t5_selling/predict.py
@@ -21,60 +21,29 @@ def main(cfg):
 
     # model
     model = T5ForConditionalGeneration.from_pretrained(cfg.PATH.save_dir)
-    model.to(device)
+    model.eval().to(device)
 
-    # convert_to_other_unicode
-    nlp = p.Nlp()
+    inputs = [
+        f"<input>{i[1]['title']}"
+        if str(i[1]["content"]) == "nan"
+        else f"<input>{i[1]['title']}\\\\{i[1]['content']}"
+        for i in df.iterrows()
+    ]
 
-    # texts = [
-    #     [
-    #         "🐰💕춘식이와 함께! 동반귀여움 춘식이 이모티콘 소장하세요\\\\춘식이가 프렌즈와 함께 등장했어요! 지금바로 소장하면 두 배로 귀여운 '춘식이 이모티콘'을 만나보세요. 고객님을 위한 애정과 감사로 가득한 소중한 선물입니다. 🎁💖",
-    #     ],
-    #     [
-    #         "🎮 퇴근 시즌 이벤트! 아이템3종과 이모티콘 무료 증정 중! (2천명 한정)\\\\\퇴근하고 싶을 때, 몰래 하는 게임이 최고예요! 지금 들어오면 아이템3종과 이모티콘을 무료로 증정해 드려요. 희소한 혜택이니 놓치지 마세요! 한정 수량이니 서둘러 참여해주세요. 🎁🕹️🎁",
-    #     ],
-    #     [
-    #         "🌞💃 핫썸머샵 원피스 위크! 최대 50% 할인 중!\\\\월요특가, 무료교환, 3종 쿠폰팩 증정! 다양한 헤택이 시원하게 쏟아지는 썸머 쇼핑 축제에서 원피스 쇼핑을 즐겨보세요. 인기와 품질을 반영한 최대 50% 세일로 마감 임박했습니다. 놓치지 마세요! 💃🛍️🎁",
-    #     ],
-    #     [
-    #         "☕️🎉 오후 3시 쿠폰 폭탄! 아메리카노 공짜로 먹을 수 있는 기회!\\\\오늘 같이 더운 날엔 아메리카노 필수니까~ 간편하고 편리한 방법으로 공짜로 아메리카노를 즐길 수 있는 기회가 오후 3시에 찾아옵니다! 지금바로 카카오 플친하러 고고해서 놓치지 마세요! ☕️🎁😊",
-    #     ],
-    # ]
-
-    # for i in texts:
-    r_list = []
-    for i in df.iterrows():
-        if {i[1]["content"]} == "":
-            text = f"<input>{i[1]['title']}"
-        else:
-            text = f"<input>{i[1]['title']}\\\\{i[1]['content']}"
-        encoded = tokenizer(nlp.convert_to_other_unicode(text))
-        sample = {k: torch.tensor([v]).to(device) for k, v in encoded.items()}
-
-        with torch.no_grad():
-            pred = model.generate(
-                **sample,
-                temperature=0.7,
-                do_sample=True,
-                top_k=10,
-                penalty_alpha=0.6,
-                max_length=512,
-                eos_token_id=tokenizer.eos_token_id,
-            )
-        # 이모지로 역치환 - Predict 후에 적용해야 함
-        result = nlp.convert_emojis_in_text(
-            nlp.convert_to_python_unicode(
-                tokenizer.decode(pred[0], skip_special_tokens=True)
-            )
+    encoded = tokenizer(inputs, padding=True, truncation=True, return_tensors="pt")
+    sample = {k: v.to(device) for k, v in encoded.items()}
+    with torch.no_grad():
+        outputs = model.generate(
+            **sample,
+            temperature=0.7,
+            do_sample=True,
+            top_k=3,
+            penalty_alpha=0.6,
+            max_length=512,
+            eos_token_id=tokenizer.eos_token_id,
         )
-        print(f"{i[0]}번째 행")
-        print(f"원문 : {text}")
-        # print(f"gpt 광고 문구 : {i[1]['sell_sent']}")
-        result = result.replace("\\\\", "\n\n").replace("\\", "\n")
-        result = result.encode("utf-8", "ignore").decode("utf-8")
-        print(f"모델 답변 : {result}")
-        print("-" * 100)
-        r_list.append(result)
+
+    r_list = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs]
 
     # save
     df["t5"] = r_list
@@ -82,7 +51,8 @@ def main(cfg):
         columns={"no": "번호", "title": "INPUT_제목", "content": "INPUT_본문"},
         inplace=True,
     )
-    fm.save(cfg.PATH.temp_save, df)
+    print(df.head())
+    fm.save(f"{cfg.PATH.temp_save}_2.xlsx", df)
 
 
 if __name__ == "__main__":
